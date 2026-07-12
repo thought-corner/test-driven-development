becaab3461c98182dc3e8163730fed74dfb9d3bf
woo jin <dnwls16071@naver.com>
2026-07-12 22:31:59 +0900

    test: use BCrypt encoder in test config

--- src/test/java/com/study/tdd/support/TestPasswordEncoderConfiguration.java
+++ src/test/java/com/study/tdd/support/TestPasswordEncoderConfiguration.java
@@ -2,23 +2,13 @@ package com.study.tdd.support;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-
-import static org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class TestPasswordEncoderConfiguration {
 
-    public static final int SALT_LENGTH = 16;
-    public static final int ITERATIONS = 10;
-
     @Bean
     @Primary
-    Pbkdf2PasswordEncoder testPasswordEncoder() {
-        return new Pbkdf2PasswordEncoder(
-            "",
-            SALT_LENGTH,
-            ITERATIONS,
-            PBKDF2WithHmacSHA256
-        );
+    BCryptPasswordEncoder testPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
