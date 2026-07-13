c9c9ddab08b73dbab33c7f9152bcbe51e7a6813e
dnwls16071 <dnwls16071@naver.com>
2026-07-13 12:06:22 +0900

    refactor: convert sign-up controller from record to class

--- src/main/java/com/study/tdd/api/controller/SellerSignUpController.java
+++ src/main/java/com/study/tdd/api/controller/SellerSignUpController.java
@@ -2,13 +2,21 @@ package com.study.tdd.api.controller;
 
 import com.study.tdd.api.controller.request.CreateSellerRequest;
 import com.study.tdd.application.SellerSignUpService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public record SellerSignUpController(SellerSignUpService sellerSignUpService) {
+public class SellerSignUpController {
+
+    private final SellerSignUpService sellerSignUpService;
+
+    @Autowired
+    public SellerSignUpController(SellerSignUpService sellerSignUpService) {
+        this.sellerSignUpService = sellerSignUpService;
+    }
 
     @PostMapping("/seller/signUp")
     ResponseEntity<?> signUp(@RequestBody CreateSellerRequest request) {
